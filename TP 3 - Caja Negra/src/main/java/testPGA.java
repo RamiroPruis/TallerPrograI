e92f01c3bbd75892013cb9b0cc815675722a3b29
Ramiro Pruis <ramiro_pruis@hotmail.com>
2021-09-23 15:09:11 -0300

    prueba

---
+++ TP 3 - Caja Negra/src/main/java/testPGA.java
@@ -0,0 +1,18 @@
+import modelo.Sistema;
+import org.junit.After;
+import org.junit.Test;
+
+public class testPGA {
+    Sistema sistema = new Sistema();
+
+
+    @After
+    public void tearDown() throws Exception{
+
+    }
+
+    @Test
+    public void keloke(){
+
+    }
+}
